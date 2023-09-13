793557e1fcd523e98ccec5b2f762177aa05c8762
Gabael33 <143562822+Gabael33@users.noreply.github.com>
2023-09-13 17:54:45 -0600

    Add files via upload

---
+++ EVA1_9_IDENTIFICADORES/src/main/java/com/mycompany/eva1_9_identificadores/EVA1_9_IDENTIFICADORES.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_9_identificadores;
+
+/**
+ *
+ * @author Gabael
+ */
+public class EVA1_9_IDENTIFICADORES {
+
+    public static void main(String[] args) {
+        //int 1nombre; nombre no valido, empieza con un numero
+        int $nombre;
+        int _nombre;
+        int nombre1;
+        int nombre;
+        int Nombre;
+        //int nombre; la variable ya existe previamente
+       
+        //int public; no podemos usar oublic, es palabra reservada (Keyword)
+        int Public;
+        double landkwenfkewnf;
+    }
+}
