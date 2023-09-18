83f1f981cf8a154b524b914b52fb94f75d0bc029
Gabael33 <143562822+Gabael33@users.noreply.github.com>
2023-09-18 17:53:07 -0600

    Add files via upload

---
+++ EVA1_10_Inicializacion/src/main/java/com/mycompany/eva1_10_inicializacion/EVA1_10_Inicializacion.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_10_inicializacion;
+
+/**
+ *
+ * @author Gabael
+ */
+public class EVA1_10_Inicializacion {
+
+    public static void main(String[] args) {
+        int valor = 0;//INICIALIZACION DE LA VARIABLE
+        System.out.println(valor);
+        int precio; //declarar
+        precio = 100; //inicializacion
+                
+        double salario = 0, precioMayoreo = 100, iva = 10.5, subtotal = 100, total = 100; //tenemos 5 variables todas del tipo doble        
+        
+      //Constantes
+      //final---Te PERMITE ASIGNAR UN VALOR SOLO UNA VEZ 
+        final int conteo = 100; 
+        System.out.println(conteo);
+        
+        final double MI_VALOR_P1 = 3.1416;
+        final String ESCUELA = "INSTITUTO TECNOLOGICO DE CHIHUAHUA II";
+        final int CALIFICACION_PASAR = 70;
+        /*
+        Varias lineas
+        CUando quieren tirar mucho rollo 
+        esta es la mejor opcion
+        */
+        int x; // COMENTARIO DE UNA SOLA LINEA, TODO EN LA LINEA DEL COMENTARIO
+      
+    }
+}
