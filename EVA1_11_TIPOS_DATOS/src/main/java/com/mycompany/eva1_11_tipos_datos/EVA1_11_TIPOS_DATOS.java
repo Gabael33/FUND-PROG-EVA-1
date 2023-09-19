6b4071af2e7c06c89e50e335a24cf02fcf304bde
Gabael33 <143562822+Gabael33@users.noreply.github.com>
2023-09-19 17:49:56 -0600

    Add files via upload

---
+++ EVA1_11_TIPOS_DATOS/src/main/java/com/mycompany/eva1_11_tipos_datos/EVA1_11_TIPOS_DATOS.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_11_tipos_datos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gabael
+ */
+public class EVA1_11_TIPOS_DATOS {
+
+    public static void main(String[] args) {
+        byte califa;
+        califa = 127;
+        //califa = 200;
+        //califa = -128;
+        //califa = -200;
+        //califa = 127+1;
+        int valor;
+        valor = Integer.MAX_VALUE;
+        System.out.println(valor+1);
+        valor = Integer.MIN_VALUE;
+        System.out.println(valor);
+        
+        //----
+        char caracter ='@';
+        System.out.println(caracter);
+        System.out.println(caracter + 0);
+        
+    }
+}
