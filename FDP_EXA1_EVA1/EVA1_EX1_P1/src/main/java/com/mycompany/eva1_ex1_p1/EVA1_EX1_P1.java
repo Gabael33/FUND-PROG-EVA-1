6a14e5ef92bdb7d3a3be4be317eaba8f87287d2e
Gabael33 <143562822+Gabael33@users.noreply.github.com>
2023-10-01 19:37:52 -0600

    Add files via upload

---
+++ FDP_EXA1_EVA1/EVA1_EX1_P1/src/main/java/com/mycompany/eva1_ex1_p1/EVA1_EX1_P1.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_ex1_p1;
+
+/**
+ *
+ * @author Gabael
+ */
+public class EVA1_EX1_P1 {
+
+    public static void main(String[] args) {
+        String NEquipo = "Team PVU";
+        String NProyecto = "Gestor de tareas";
+        String NInt1 = "Pedro Gabael";
+        String NInt2 = "Uriel";
+        String NInt3 = "Alfredo";
+        String Aint1 = "Espinoza de la Rosa";
+        String Aint2 = "Frias Medina";
+        String Aint3 = "Venzor Garcia";
+        String NCInt1 = "23550316";
+        String NCInt2 = "23550325";
+        String NCInt3 = "23550353";
+        //Declaramos todas las varibles.
+        
+        System.out.println("El nombre del equipo es:" + NEquipo);
+        System.out.println("Nombre del proyecto final:" + NProyecto);
+        System.out.println("Integrante 1:" + Aint1 + " " + NInt1+ " " + NCInt1);
+        System.out.println("Integrante 2:" + Aint2 + " " + NInt2 + " " + NCInt2);
+        System.out.println("Integrante 3:" + Aint3 + " " + NInt3 + " " + NCInt3);
+        //Imprimimos todas las variables para mostrar el mensaje.
+       
+    }
+}
